e879fd65f3358d23d70ccffe7da73ac5676e7111
Kundu-A <kunduarpan43@gmail.com>
2025-01-15 23:35:55 +0530

    Some data types of javascript is discussed

---
+++ JAVASCRIPT_TUITORIAL/datatypes.js
@@ -0,0 +1,26 @@
+let person="Arpan" //String
+let age=22  //Number
+let isloggedIn=true // Boolean
+console.table([person,age,isloggedIn])
+
+//alert(3+3) //It is not usefull for nodeJs , it's allowed only browser.
+
+/*
+Primitive Data Types:
+String => ""/''
+Number => range = 2 to the power 53
+For very large number we can use BigInt
+Boolean=> true/false
+null => Standalone value
+undefined 
+symbol => unique
+
+********************************
+
+object
+ */
+console.log(typeof "Arpan")
+console.log(typeof age)
+console.log(typeof(isloggedIn))// There are two type to use typeof.
+console.log(typeof null) // null is an object
+console.log(typeof undefined) // "undefined" is catagorized as "undefined"
