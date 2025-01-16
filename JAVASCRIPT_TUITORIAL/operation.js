1286bda0286e55f1f64eb591586302e6f86d49c2
Kundu-A <kunduarpan43@gmail.com>
2025-01-16 12:53:19 +0530

    Perform some basic operations

---
+++ JAVASCRIPT_TUITORIAL/operation.js
@@ -0,0 +1,29 @@
+let value=3;
+let negValue=-3;
+console.log(negValue);
+
+console.log(3+2); // Addition Operation
+console.log(3-2); // Subtraction Operation
+console.log(3*2); // Multiplication Operation
+console.log(3**2); // Power operation
+console.log(3/2); // Division Operation
+console.log(3%2); // Modulus Operation
+
+let str1="Hello " 
+let str2="Arpan!!"
+let str3=str1+str2;
+console.log(str3); // Concatenation Operation
+
+console.log("1"+2); // Oputput = 12
+console.log(2+"1"); // Outpur = 21
+console.log("1"+2+2); // Output  = 122
+console.log(1+2+"2"); // output = 32
+
+let num1,num2,num3;
+num1=num2=num3=4; // Assignment Operation
+console.table([num1 , num2 , num3]);
+
+let counter=0;
+console.log(counter++);
+console.log(counter); // Postfix operation
+console.log(++counter); //Prefix Operation
