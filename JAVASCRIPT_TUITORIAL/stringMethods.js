4a765c518377323f203f541ae0d5dbbaf16608e4
Kundu-A <kunduarpan43@gmail.com>
2025-02-12 00:26:23 +0530

    Add some string related methods

--- JAVASCRIPT_TUITORIAL/stringMethods.js
+++ JAVASCRIPT_TUITORIAL/stringMethods.js
@@ -16,3 +16,26 @@ console.log(str1.endsWith("student"))
 console.log(str.lastIndexOf("am"))
 console.log(str.lastIndexOf("arpan")) // it will return -1 because "arpan" is not present str.
 
+//endsWith() method will return true if the specific element is present at the end of the string otherwise return false.
+console.log(str.endsWith("student"));//true
+console.log(str.endsWith("students"));//false
+
+//match() method is used to match a string belongs to a specific pattern.
+const regex=/[A-Z]/g
+console.log(str.match(regex));
+
+//repeat() method will used to use an element multiple times
+const mood="Happy "
+console.log(`I am very ${mood.repeat(3)}`);
+
+//replace() and replaceAll() methods are used to replace any element with and exsisting element.
+console.log(str.replace("am" , "was"));
+const para="Cat is the best animal in the world , having a Cat is blessing"
+console.log(para.replaceAll("Cat","Dog"));
+
+//substring() method is used to take a sub-string from a original string index wise.
+console.log(str.substring(1,5));
+console.log(str.substring(3));
+
+//length is the property of the string to calculate the  length of the string.
+console.log(str.length)
